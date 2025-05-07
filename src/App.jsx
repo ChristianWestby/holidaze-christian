92ad645b803884d2d80924845069101c9906bcc4
Christian Andreas Westby <144798762+ChristianWestby@users.noreply.github.com>
2025-05-07 15:11:35 +0200

    Endringer for bedre funksjon.

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import VenueDetail from "./pages/VenueDetail";
+import { Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import VenueDetail from './pages/VenueDetail';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <>
       <nav className="p-4 bg-gray-100 flex gap-4">
-        <a href="/" className="text-blue-600 underline">Hjem</a>
-        <a href="/about" className="text-blue-600 underline">Om</a>
+        <Link to="/" className="text-blue-600 underline">Hjem</Link>
+        <Link to="/about" className="text-blue-600 underline">Om</Link>
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/venue/:id" element={<VenueDetail />} />
       </Routes>
-    </BrowserRouter>
+    </>
   );
 }
