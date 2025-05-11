9d0394e7d1fddba474c8f6f48ccf54ccd0347904
Christian Andreas Westby <144798762+ChristianWestby@users.noreply.github.com>
2025-05-12 00:09:18 +0200

    Lagde login og register.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import VenueDetail from "./pages/VenueDetail"; 
+import Login from "./auth/Login";
+import Register from "./auth/Register";
 
 export default function App() {
   return (
@@ -12,6 +14,8 @@ export default function App() {
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="venue/:id" element={<VenueDetail />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
         </Route>
       </Routes>
     </BrowserRouter>
