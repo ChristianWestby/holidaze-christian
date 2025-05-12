01dd581a67fcf7d5ed789474ba78f1f5b2b070c7
Christian Andreas Westby <144798762+ChristianWestby@users.noreply.github.com>
2025-05-12 02:41:27 +0200

    Ferdig med registrer med redirect og login.

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,33 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import VenueDetail from "./pages/VenueDetail"; 
-import Login from "../auth/Login";
-import Register from "../auth/Register";
+import VenueDetail from "./pages/VenueDetail";
+import Profile from "./pages/Profile";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import ProtectedRoute from "./utils/ProtectedRoute";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="venue/:id" element={<VenueDetail />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route
+            path="profile"
+            element={
+              <ProtectedRoute>
+                <Profile />
+              </ProtectedRoute>
+            }
+          />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
