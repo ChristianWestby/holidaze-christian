b87d07d0b8feefd3dc7d1e0f56e766dbd6da3fe9
Christian Andreas Westby <144798762+ChristianWestby@users.noreply.github.com>
2025-05-22 19:06:34 +0200

    Footer sticky.

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,15 +1,15 @@
-import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import { Outlet } from "react-router-dom";
 
 export default function Layout() {
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
       <Navbar />
-      <main>
+      <main className="flex-grow">
         <Outlet />
       </main>
-      <Footer /> {/* Erstattet den gamle <footer> */}
-    </>
+      <Footer />
+    </div>
   );
 }
